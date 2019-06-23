642c0a147c8ee3556dae25aad899574d2c053fa3
Zaccari S <zacosx@gmail.com>
2019-06-23 04:00:40 -0400

    Initial Commit

---
+++ Conflux-Client/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Conflux-Client
+//
+//  Created by Zaccari Silverman on 6/23/19.
+//  Copyright © 2019 macosten. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Conflux-Client/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Conflux-Client
+//
+//  Created by Zaccari Silverman on 6/23/19.
+//  Copyright © 2019 macosten. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
