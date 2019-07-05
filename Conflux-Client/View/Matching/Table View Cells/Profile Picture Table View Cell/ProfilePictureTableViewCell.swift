0a8ce8cd5fd62cc464e8cde4859084ba9bfd25ea
Zaccari S <zacosx@gmail.com>
2019-07-05 13:43:29 -0400

    Added files for TableViewCell types in anticipation of modeling the MatchingViewController off that "Baseball Card" idea. Not everyone may have all possible data filled in, so we need a format that doesn't choke and die if some of the optional data doesn't exist.

---
+++ Conflux-Client/View/Matching/Table View Cells/Profile Picture Table View Cell/ProfilePictureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfilePictureTableViewCell.swift
+//  Conflux-Client
+//
+//  Created by Zaccari Silverman on 7/5/19.
+//  Copyright © 2019 macosten. All rights reserved.
+//
+
+import UIKit
+
+class ProfilePictureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
