f45a7924b3bd6c2ad2a2a4cad96e7071cceb733f
Zaccari S <zacosx@gmail.com>
2020-01-04 20:50:40 -0500

    Connected CreatePostViewController storyboard to code stubs.

--- Basin-Client/Controller/CreatePostViewController.swift
+++ Basin-Client/Controller/CreatePostViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class CreatePostViewController: UIViewController {
 
+    @IBOutlet weak var titleLengthLabel: UILabel!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var textLengthLabel: UILabel!
+    
+    @IBOutlet weak var textTextView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func postButtonWasPressed(_ sender: Any) {
+            presentDialogBox(withTitle: "Error", withMessage: "Posting is not yet supported.")
     }
-    */
+    
+    
 
 }
